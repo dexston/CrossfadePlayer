9fb568246da7a24975dd29e822948d317e962417
Dexston <mrdexston@gmail.com>
2022-04-12 01:20:47 +0300

    Reworked UI

--- CrossfadePlayer/Utils/AppConstants.swift
+++ CrossfadePlayer/Utils/AppConstants.swift
@@ -6,17 +6,25 @@
 //
 
 import Foundation
+import UIKit
 
 struct K {
     
     static let title = "Crossfade player"
-    static let padding = 30.0
+    static let padding = 15.0
+    static let currentFadeValue = "Current fade duration:  %.f"
     
     struct Button {
-        static let play = "Play"
-        static let stop = "Stop"
+        static let play = UIImage(systemName: "play.fill")
+        static let stop = UIImage(systemName: "stop.fill")
         static let firstSound = "Select first sound"
         static let secondSound = "Select second sound"
+    }
+    
+    struct FadeSlider {
+        static let minValue: Float = 2.0
+        static let maxValue: Float = 10.0
+        static let initialValue: Float = 4.0
     }
     
     enum Error: String {
